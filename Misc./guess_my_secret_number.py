8c60987b1ddf4d709085065208c69ed9f8ca212f
AndrewHuff6 <77467984+AndrewHuff6@users.noreply.github.com>
2026-08-27 21:07:07 +0000

    guessing game example from class

--- Misc./guess_my_secret_number.py
+++ Misc./guess_my_secret_number.py
@@ -1,1 +1,19 @@
+import random
 
+secret = int(random.randint(1,6))
+guess = int(input("Guess a number: "))
+count = 0
+
+if secret != guess:
+    if guess > secret:
+        print("Too high!")
+        count += 1
+    elif guess < secret:
+        print("Too low!")
+        count += 1
+else:
+    count += 1
+    print(f"You guessed correctly! It took you {count} guesses.")
+
+#for i in range(3):
+#    pass
