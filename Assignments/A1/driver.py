27113adcf9efa6896a4b41da75187af39ae48b4b
AndrewHuff6 <77467984+AndrewHuff6@users.noreply.github.com>
2026-09-11 19:44:02 +0000

    created list functionality for offense and defense

--- Assignments/A1/driver.py
+++ Assignments/A1/driver.py
@@ -1,7 +1,10 @@
 # Football Game Simulation using Python
 # Andrew Huff - 5383 Advanced OOP
 
-from players import .   # imports all five position players to driver
+# imports all position players to driver
+from players import QuarterBack, RunningBack, WideReceiver, Deflineman, LineBacker, CornerBack   
+# from play_game import .
+# from team_info import .
 
 # BEGINNING OF PROGRAM RUN
 print("Welcome to Andrew's Football Game Simulator!")
@@ -11,13 +14,37 @@ choice = input("Press space -> enter to start the game...")
 if choice != " ":
     print("Goodbye!")
 else:
+    print("-" * 41)
     print("Let's play some football!")
-    print("Loading game...")
+    print("Creating teams...")
+    print("-" * 41)
 
 # Create a list of players for the game
 offense_players = []
 defense_players = []
 
+# Players on Offense
+qb = QuarterBack("Andrew")
+rb = RunningBack("John")
+wr = WideReceiver("Mike")
 
+# Players on Defense
+dl = Deflineman("James")
+lb = LineBacker("David")
+cb = CornerBack("Chris")
 
+offense_players.append(qb)  # Add the quarterback to the offense players list
+offense_players.append(rb)  # Add the running back to the offense players list
+offense_players.append(wr)  # Add the wide receiver to the offense players list
 
+defense_players.append(dl)  # Add the defensive lineman to the defense players list
+defense_players.append(lb)  # Add the line backer to the defense players list
+defense_players.append(cb)  # Add the corner back to the defense players list
+
+print("Offense:")
+for player in offense_players:
+    print(f" Player Name: {player.name}, Position: {player.position}, Speed: {player.speed}, Strength: {player.strength}, Stamina: {player.stamina}")
+
+print("Defense:")
+for player in defense_players:
+    print(f" Player Name: {player.name}, Position: {player.position}, Speed: {player.speed}, Strength: {player.strength}, Stamina: {player.stamina}")
--- Assignments/A1/players.py
+++ Assignments/A1/players.py
@@ -2,7 +2,7 @@
 
 # Super class definition for all players in the game.
 class Player:
-    def __init(self, name, position, speed, strength, stamina):
+    def __init__(self, name, position, speed, strength, stamina):
         self.name = name
         self.position = position
         self.strength = strength
@@ -14,6 +14,9 @@ class QuarterBack(Player):
     def __init__(self, name, speed=60, strength=70, stamina=75):
         super().__init__(name, "Quarterback", speed, strength, stamina)
 
+    def hello(self):
+        pass
+
 # Running back class definition for all RBs in the game.
 class RunningBack(Player):
     def __init__(self, name, speed=80, strength=90, stamina=85):
@@ -24,12 +27,17 @@ class WideReceiver(Player):
     def __init__(self, name, speed=90, strength=65, stamina=80):
         super().__init__(name, "Wide receiver", speed, strength, stamina)
 
+# Defensive lineman class definition for all DLs in the game.
+class Deflineman(Player):
+    def __init__(self, name, speed=55, strength=95, stamina=70):
+        super().__init__(name, "D-Lineman", speed, strength, stamina)
+
 # Linebacker class definition for all LBs in the game.
 class LineBacker(Player):
     def __init__(self, name, speed=70, strength=90, stamina=75):
         super().__init__(name, "Linebacker", speed, strength, stamina)
 
 # Cornerback class definition for all CBs in the game.
-class Cornerback(Player):
+class CornerBack(Player):
     def __init__(self, name, speed=90, strength=70, stamina=75):
         super().__init__(name, "Cornerback", speed, strength, stamina)
